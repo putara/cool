d10a793935b6442050fc3bbb7e13b8c7f4f867f6
putara <38089082+putara@users.noreply.github.com>
2018-07-09 22:53:12 +1200

    Remove redundant code

--- Controls/LinkLabel.cs
+++ Controls/LinkLabel.cs
@@ -456,25 +456,6 @@ namespace Cool
             }
         }
 
-        // copied from CaptionBar.cs.
-        public static Color BlendColours(Color baseColour, Color overlayColour, float alpha)
-        {
-            if (alpha == 0)
-            {
-                return overlayColour;
-            }
-            else if (alpha == 1)
-            {
-                return baseColour;
-            }
-
-            var ovlAlpha = 1f - alpha;
-            return Color.FromArgb(
-                    (int)(baseColour.R * alpha + overlayColour.R * ovlAlpha),
-                    (int)(baseColour.G * alpha + overlayColour.G * ovlAlpha),
-                    (int)(baseColour.B * alpha + overlayColour.B * ovlAlpha));
-        }
-
         Color CalculateShadowColour(Color colour)
         {
             Color backColour = this.BackColor;
@@ -483,55 +464,7 @@ namespace Cool
                 backColour = this.Parent.BackColor;
             }
             colour = colour.GetBrightness() > .5f ? Color.Black : Color.White;
-            return BlendColours(colour, backColour, .5f);
+            return ColourUtils.Blend(colour, backColour, .5f);
         }
-
-        //static Color CalculateShadowColour(Color colour)
-        //{
-        //  float hue = colour.GetHue();
-        //  float saturation = colour.GetSaturation();
-        //  float brightness = colour.GetBrightness();
-        //  if (brightness < .5f)
-        //  {
-        //      brightness = Math.Min(1, brightness + .75f);
-        //  }
-        //  else
-        //  {
-        //      brightness = Math.Max(0, brightness - .75f);
-        //  }
-        //  return FromHSB(hue, saturation, brightness);
-        //}
-
-        //// https://en.wikipedia.org/wiki/HSL_and_HSV#From_HSL
-        //static Color FromHSB(float hue, float saturation, float brightness)
-        //{
-        //  float c = brightness * saturation;
-        //  float h = hue * 6f;
-        //  float x = c * (1 - Math.Abs(h % 2 - 1));
-        //  float r, g, b;
-
-        //  switch ((int)Math.Floor(h))
-        //  {
-        //      case 0:
-        //          r = c; g = x; b = 0; break;
-        //      case 1:
-        //          r = x; g = c; b = 0; break;
-        //      case 2:
-        //          r = 0; g = c; b = x; break;
-        //      case 3:
-        //          r = 0; g = x; b = c; break;
-        //      case 4:
-        //          r = x; g = 0; b = c; break;
-        //      case 5:
-        //      default:
-        //          r = c; g = 0; b = x; break;
-        //  }
-
-        //  float m = brightness - c;
-        //  return Color.FromArgb(
-        //          (int)Math.Floor((r + m) * 255),
-        //          (int)Math.Floor((g + m) * 255),
-        //          (int)Math.Floor((b + m) * 255));
-        //}
     }
 }
